0039458c9df9ebb1f5dac094521236f7949f2c8c
Ka1bi4 <rrrrrroman@gmail.com>
2019-07-19 14:13:08 +0300

    Test project
    Test project files upload

---
+++ oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,4 @@
+fil = new Array();
+fil["0"]= "overview.html@@@Install CodeLobster@@@...";
+fil["1"]= "welcome.html@@@Meet CodeLobster@@@...";
+var doStem = true;searchLoaded = true;
---
+++ oxygen-webhelp/search/keywords.js
@@ -0,0 +1,4 @@
+var keywords=[{w:"Install",p:["p0"]},{w:"CodeLobster",p:["p0","p1"]},{w:"Meet",p:["p1"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 1];
