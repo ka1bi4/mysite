9746eaef47099daf80ac2854ef56581bbe31d688
Ka1bi4 <rrrrrroman@gmail.com>
2019-07-19 16:59:28 +0300

    Make links

--- oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20190719021600", "oxygen-webhelp/search/index-1.js?uniqueId=20190719021600", "oxygen-webhelp/search/index-2.js?uniqueId=20190719021600", "oxygen-webhelp/search/index-3.js?uniqueId=20190719021600"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20190719045836", "oxygen-webhelp/search/index-1.js?uniqueId=20190719045836", "oxygen-webhelp/search/index-2.js?uniqueId=20190719045836", "oxygen-webhelp/search/index-3.js?uniqueId=20190719045836"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
--- oxygen-webhelp/search/htmlFileInfoList.js
+++ oxygen-webhelp/search/htmlFileInfoList.js
@@ -1,4 +1,4 @@
 fil = new Array();
-fil["0"]= "overview.html@@@Install CodeLobster@@@...";
+fil["0"]= "install.html@@@Install CodeLobster@@@...";
 fil["1"]= "welcome.html@@@Meet CodeLobster@@@...";
 var doStem = true;searchLoaded = true;
